60c05015dea4f5d9f58e022197c36967e60d7b15
Dan Nguyen <dansonguyen@gmail.com>
2015-10-20 17:23:27 -0700

    js

--- mumbai.js
+++ mumbai.js
@@ -1,9 +1,21 @@
+function party_name_to_class(x){
+    return x.replace(/ /g, '_');
+}
+
+
+var PARTY_ATTRIBUTES = ["Constituency", "Total_assets", "Total_Cases",
+"Murder_charges", "Kidnapping_charges", "Dacoity_charges", "Crimes_women"]
+
+
 $(document).ready(function () {
     var data = datatable['data'];
+
+    $('#info_box').hide();
+
     for (var i = 0; i < data.length; i++) {
         //iterate to bind svg with data
         var obj = data[i];
-        var o_id = (obj['Constituency']).replace(/ /g, '_');
+        var o_id = party_name_to_class(obj['Constituency']);
         $('#' + o_id).data({
             'Candidate': obj['Candidate'],
             'Constituency': obj['Constituency'],
@@ -17,19 +29,35 @@ $(document).ready(function () {
         });
     }
 
+
     $(".mumbai").mouseover(function (e) {
-        var info = "<b>" + $(this).data("Candidate") + "</b>" +
-        "<br>" + "<b>" + $(this).data("Party") + "</b>" +
-        "<br> Constituency: " + $(this).data("Constituency") + 
-        "<br> Total assets: " + $(this).data("Total_assets") + " crore" +
-        "<br> Total cases: " + $(this).data("Total_Cases") +
-        "<br> Murder charges: " + $(this).data("Murder_charges") + 
-        "<br> Kidnapping charges: " + $(this).data("Kidnapping_charges") +
-        "<br> Dacoity charges: " + $(this).data("Dacoity_charges") +
-        "<br> Crimes against women: " + $(this).data("Crimes_women");
-        
-        $('#info_box').html(info);
-	});
-    
-    
+        $('#info_box').show(300)
+
+        var item = $(this);
+        $('#info_box .party-name').addClass( party_name_to_class(item.data("Party")));
+        $('#info_box .candidate-name').text(item.data("Candidate"));
+        $('#info_box .party-name').text(item.data("Party"));
+        for(var i = 0, x = PARTY_ATTRIBUTES.length; i < x; i++){
+            var att = PARTY_ATTRIBUTES[i];
+            var val = item.data(att);
+            $("#info_table ." + att + ' .value').text(val);
+        }
+
+    });
+
 })
+
+
+        // var info = "<b>" + $(this).data("Candidate") + "</b>" +
+        // "<h5 class='party-name'>" + $(this).data("Party") + "</h5>" +
+        // "<table>" +
+        // "</table>"
+
+        // "<br> Constituency: " + $(this).data("Constituency") +
+        // "<br> Total assets: " + $(this).data("Total_assets") + " crore" +
+        // "<br> Total cases: " + $(this).data("Total_Cases") +
+        // "<br> Murder charges: " + $(this).data("Murder_charges") +
+        // "<br> Kidnapping charges: " + $(this).data("Kidnapping_charges") +
+        // "<br> Dacoity charges: " + $(this).data("Dacoity_charges") +
+        // "<br> Crimes against women: " + $(this).data("Crimes_women");
+
