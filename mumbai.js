5e570c50f35b092b1c95e08aa5b3ccebebd87000
Saurabh Datar <sdatar2@stanford.edu>
2015-10-21 19:19:28 -0700

    Viz done

--- mumbai.js
+++ mumbai.js
@@ -2,7 +2,6 @@ function party_name_to_class(x){
     return x.replace(/ /g, '_');
 }
 
-
 var PARTY_ATTRIBUTES = ["Constituency", "Total_assets", "Total_Cases",
 "Murder_charges", "Kidnapping_charges", "Dacoity_charges", "Crimes_women"]
 
@@ -13,7 +12,6 @@ $(document).ready(function () {
     $('#info_box').hide();
 
     for (var i = 0; i < data.length; i++) {
-        //iterate to bind svg with data
         var obj = data[i];
         var o_id = party_name_to_class(obj['Constituency']);
         $('#' + o_id).data({
@@ -31,12 +29,12 @@ $(document).ready(function () {
 
 
     $(".mumbai").mouseover(function (e) {
-        $('#info_box').show(300)
+        $('#info_box').show(300);
 
         var item = $(this);
         $('#info_box .party-name').addClass( party_name_to_class(item.data("Party")));
-        $('#info_box .candidate-name').text(item.data("Candidate"));
-        $('#info_box .party-name').text(item.data("Party"));
+        $('#info_box .candidate-name').html("<b>" + item.data("Candidate") + "</b>");
+        $('#info_box .party-name').html("<b>" + item.data("Party") + "</b>");
         for(var i = 0, x = PARTY_ATTRIBUTES.length; i < x; i++){
             var att = PARTY_ATTRIBUTES[i];
             var val = item.data(att);
@@ -46,18 +44,3 @@ $(document).ready(function () {
     });
 
 })
-
-
-        // var info = "<b>" + $(this).data("Candidate") + "</b>" +
-        // "<h5 class='party-name'>" + $(this).data("Party") + "</h5>" +
-        // "<table>" +
-        // "</table>"
-
-        // "<br> Constituency: " + $(this).data("Constituency") +
-        // "<br> Total assets: " + $(this).data("Total_assets") + " crore" +
-        // "<br> Total cases: " + $(this).data("Total_Cases") +
-        // "<br> Murder charges: " + $(this).data("Murder_charges") +
-        // "<br> Kidnapping charges: " + $(this).data("Kidnapping_charges") +
-        // "<br> Dacoity charges: " + $(this).data("Dacoity_charges") +
-        // "<br> Crimes against women: " + $(this).data("Crimes_women");
-
